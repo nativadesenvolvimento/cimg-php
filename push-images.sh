eca022619a30f262eedb0aee818c6f7ed97cf7c0
Lucas Romano <lucasromanojf@gmail.com>
2022-02-21 16:18:35 -0300

    Customizations

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/php:8.0.16
-docker push cimg/php:8.0
-docker push cimg/php:8.0.16-node
-docker push cimg/php:8.0-node
-docker push cimg/php:8.0.16-browsers
-docker push cimg/php:8.0-browsers
+docker push nativa/circle-ci-php:8.1.3
+docker push nativa/circle-ci-php:8.1
+docker push nativa/circle-ci-php:8.1.3-node
+docker push nativa/circle-ci-php:8.1-node
+docker push nativa/circle-ci-php:8.1.3-browsers
+docker push nativa/circle-ci-php:8.1-browsers
